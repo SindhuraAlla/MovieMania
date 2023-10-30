b87a1543345de012f24e0fed590f33539ecff1df
Sindhura Alla <111253789+SindhuraAlla@users.noreply.github.com>
2023-10-29 21:21:15 -0400

    Javascript for movies table rendering

--- js/movies.js
+++ js/movies.js
@@ -1,5 +1,106 @@
 import { getMoviesData, getGenreListData, getMoviedCountByGenre } from "./fetch-api-data.js";
 
+async function renderMoviesTableBody() {
+  const with_genres = document
+    .getElementById("genreDropdownMenuSelected")
+    .getAttribute("data-genreDropdownMenuSelected");
+  const sort_by = document
+    .getElementById("sortDropdownMenuSelected")
+    .getAttribute("data-sortDropdownMenuSelected");
+
+  const data = await getMoviesData({ sort_by, with_genres });
+  const movies = data?.results ?? [];
+
+  const moviesTableBody = document.getElementById("moviesTableBody");
+
+  let counter = 1;
+
+  const rows = movies.reduce((rows, movie) => {
+    rows.push(
+      `<tr>
+        <th scope="row">${counter}</th>
+        <td>${movie.original_title}</td>
+        <td>
+          <a href="movie_details.html?id=${movie.id}" class="link-underline-light">
+            <span class="icon-color">&#128279</span>
+          </a>
+        </td>
+        <td>${movie.vote_average}</td>
+        <td>${movie.vote_count}</td>
+        <td>${movie.release_date}</td>
+      </tr>
+    `
+    );
+    counter++;
+    return rows;
+  }, []);
+
+  moviesTableBody.innerHTML = rows.join(" ");
+}
+
+function onGenreChange(name, id) {
+  const element = document.getElementById("genreDropdownMenuSelected");
+  element.innerText = name;
+  element.setAttribute("data-genreDropdownMenuSelected", id);
+
+  renderMoviesTableBody();
+}
+
+async function renderDropDownSource() {
+  const dropdownMenuSource = document.getElementById("genreDropdownMenuSource");
+
+  const genres = (await getGenreListData()) ?? [];
+
+  for (const genre of genres) {
+    const element = document.createElement("button");
+    element.classList.add("dropdown-item");
+    element.appendChild(document.createTextNode(genre.name));
+    dropdownMenuSource.appendChild(element);
+    element.onclick = function () {
+      onGenreChange(genre.name, genre.id);
+    };
+  }
+}
+
+const SORT_ELEMENTS_INNER_HTML = {
+  "popularity.desc": `Rating <span class="sort-icon-color">&#8593;</span>`,
+  "popularity.asc": `Rating <span class="sort-icon-color">&#8595;</span>`,
+  "votes.desc": `Votes <span class="sort-icon-color">&#8593;</span>`,
+  "votes.asc": `Votes <span class="sort-icon-color">&#8595;</span>`,
+  "primary_release_date.desc": `Release Date <span class="sort-icon-color">&#8593;</span>`,
+  "primary_release_date.asc": `Release Date <span class="sort-icon-color">&#8595;</span>`,
+};
+
+function onSortChange(value) {
+  const element = document.getElementById("sortDropdownMenuSelected");
+  element.innerHTML = SORT_ELEMENTS_INNER_HTML[value] ?? "";
+  element.setAttribute("data-sortDropdownMenuSelected", value);
+
+  renderMoviesTableBody();
+}
+
+function renderSortDropdownMenuSource() {
+  const dropdownMenuSource = document.getElementById("sortDropdownMenuSource");
+
+  const sortables = Object.entries(SORT_ELEMENTS_INNER_HTML);
+
+  for (const [sort_by, innerHtml] of sortables) {
+    const element = document.createElement("button");
+    element.classList.add("dropdown-item");
+    element.innerHTML = innerHtml;
+    dropdownMenuSource.appendChild(element);
+    element.onclick = function () {
+      onSortChange(sort_by);
+    };
+  }
+}
+
+(function () {
+  renderDropDownSource();
+  renderSortDropdownMenuSource();
+  renderMoviesTableBody();
+})();
+
 
 function getRandomColor() {
   const r = Math.floor(Math.random() * 256);
